24e5a64bd7d68ec1db014b50a75a869f8c8330d3
denisorlyk20@gmail.com <denisorlyk20@gmail.com>
2026-09-17 16:11:42 +0300

    declaration division function in header file and its implementation in source code file, add main function

--- calculator.cpp
+++ calculator.cpp
@@ -2,7 +2,7 @@
 
 int Calculator::Add (double a, double b)
 {
-	return a + b + 0.5;
+    return a + b + 0.5;
 }
 
 int Calculator::Sub (double a, double b)
@@ -13,4 +13,22 @@ int Calculator::Sub (double a, double b)
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
+}
+
+int Calculator::Div (double a, double b);
+{
+    return a / b + 0.5;
+}
+
+int main(int argc, char** argv) {
+    double a = 10.0, b = 2.0;
+
+    Calculator calc;
+
+    std::cout << calc.Add(a, b) << std::endl;
+    std::cout << calc.Sub(a, b) << std::endl;
+    std::cout << calc.Mul(a, b) << std::endl;
+    std::cout << calc.Div(a, b) << std::endl;
+
+    return 0;
 }
--- calculator.h
+++ calculator.h
@@ -6,6 +6,8 @@ class Calculator
     public:
         int Add (double, double);
         int Sub (double, double);
+	int Mul (double, double);
+	int Div (double, double);
 };
 
 #endif//CALCULATOR_H
